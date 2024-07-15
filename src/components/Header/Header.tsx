4cacf23b5295fb8ca12cd3c8ac028de752cc6b0e
AnshLokhande <priyanshulokhande72@gmail.com>
2024-07-15 14:27:06 +0530

    Update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,8 @@ import { Container } from './styles'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { NavHashLink, HashLink } from 'react-router-hash-link'
 import { useState } from 'react'
-import Resume from '../../assets/priyanshu resume.pdf'
+import Resume from '../../assets/priyanshu_resume.pdf'
+
 export function Header() {
   const [isActive, setActive] = useState(false)
   function toggleTheme() {
